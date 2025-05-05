d133436ac993d43616e9ddfd014b7976697734c9
Joao Cardoso <jaliddons@gmail.com>
2025-05-05 19:09:26 +0100

    Addon custom defaults

--- src/defaults.lua
+++ src/defaults.lua
@@ -1,6 +1,52 @@
 --[[
-	Default settings specific to Bagnonium.
+	Bagnonium default settings.
 	All Rights Reserved
 --]]
 
 local ADDON, Addon = ...
+local function AsArray(table)
+	return setmetatable(table, {__metatable = false})
+end
+
+local FrameDefaults = {
+	enabled = true,
+	sidebar = true, tabs = true, bagToggle = true,
+
+	filters = AsArray({'all', 'reagent', 'consumable', 'armor', 'questitem', 'miscellaneous'}),
+	brokerObject = ADDON .. 'Launcher',
+	skin = 'Combuctor',
+	
+	strata = 'HIGH', alpha = 1, scale = 1,
+	point = 'TOPLEFT',
+	x = 15, y = -115,
+
+	bagBreak = 1, breakSpace = 1.3, spacing = 2,
+	itemScale = 1.5,
+
+	color = {PANEL_BACKGROUND_COLOR:GetRGBA()},
+	borderColor = {1, 1, 1, 1},
+}
+
+Addon.Settings.ProfileDefaults = {
+	inventory = Addon:SetDefaults({
+		width = Addon.IsRetail and 485 or 400, height = Addon.IsRetail and 680 or 500,
+		point = 'BOTTOMRIGHT',
+		x = -13, y = 104,
+
+		skin = Addon.Skins.Default,
+		currency = true,
+	}, FrameDefaults),
+
+	bank = Addon:SetDefaults({
+		width = Addon.IsRetail and 927 or 694, height = Addon.IsRetail and 674 or 436,
+		deposit = true, currency = true
+	}, FrameDefaults),
+
+	guild = Addon:SetDefaults({
+		width = 637, height = 448,
+	}, FrameDefaults),
+
+	vault = Addon:SetDefaults({
+		width = 927, height = 674
+	}, FrameDefaults)
+}
--- src/skins.lua
+++ src/skins.lua
@@ -37,7 +37,7 @@ if Addon.IsRetail then
 	Addon.Skins:Register {
 		id = 'Bagnonium', template = 'PortraitFrameFlatTemplate', closeX = 3, closeY = 2,
 		borderColor = function(skin,...) NineSlicePanelMixin.SetBorderColor(skin.NineSlice,...) end,
-		--centerColor = function(skin, ...) skin:SetBackgroundColor(CreateColor(...)) end, PANEL_BACKGROUND_COLOR
+		centerColor = function(skin, ...) skin:SetBackgroundColor(CreateColor(...)) end,
 		load = function(skin)
 			skin.Bg:SetFrameLevel(0)
 			skin.CloseButton:Hide()
